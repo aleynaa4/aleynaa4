3d3772914d5acd952ff96726d52918229e940181
aleynaa4 <107534689+aleynaa4@users.noreply.github.com>
2022-06-19 09:42:28 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿const config = {
+  "botStatus": ["Developed By Aleyna"],
+  "prefix": ["!", "."],
+  "token": "OTgzNDQxNzIzMDM0MzM3MzIw.G9NoVD.hPS8ZNhtvuSPl2flV5I_76vMCoFPtkGLVhpbkM",
+  "mongoose": "mongodb+srv://aleyna:aleynadc1234@cluster0.pgf7z.mongodb.net/aleynacikramalisteaq?retryWrites=true&w=majority",
+  "botOwners": ["921430931947003925"],
+  "guildID": "987804472967315538",
+  
+  "emojis": {
+    "yes_name": "yes_ramal",
+    "no_name": "no_ramal",
+    "mute_name": "ramal_mute"
+  }
+  }
+  
+  module.exports = config;
+  
